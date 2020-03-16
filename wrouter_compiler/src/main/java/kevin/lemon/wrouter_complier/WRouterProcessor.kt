cdf74762681ae167a3c3b85ff48b1ca45b4a9a59
kevin <765002108@qq.com>
2020-03-16 16:34:13 +0800

    WRouter

--- wrouter_compiler/src/main/java/kevin/lemon/wrouter_complier/WRouterProcessor.kt
+++ wrouter_compiler/src/main/java/kevin/lemon/wrouter_complier/WRouterProcessor.kt
@@ -1,32 +1,29 @@
 package kevin.lemon.wrouter_complier
 
-import com.google.auto.service.AutoService
+import kevin.lemon.wrouter_annotation.WRouter
 import kevin.lemon.wrouter_annotation.bean.RouterBean
 import kevin.lemon.wrouter_complier.utils.ProcessorConfig
 import java.util.*
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
+import javax.lang.model.element.Element
 import javax.lang.model.element.TypeElement
 import javax.lang.model.util.Elements
 import javax.lang.model.util.Types
 import javax.tools.Diagnostic
-
 /**
  * Created by wxk on 2020/3/16.
  */
 // AutoService则是固定的写法，加个注解即可
 // 通过auto-service中的@AutoService可以自动生成AutoService注解处理器，用来注册
 // 用来生成 META-INF/services/javax.annotation.processing.Processor 文件
-@AutoService(Processor::class) // 允许/支持的注解类型，让注解处理器处理
-
 // 指定JDK编译版本
-@SupportedSourceVersion(SourceVersion.RELEASE_7)
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
 // 允许/支持的注解类型，让注解处理器处理
 @SupportedAnnotationTypes(ProcessorConfig.AROUTER_PACKAGE)
-
 // 注解处理器接收的参数
 @SupportedOptions(ProcessorConfig.OPTIONS, ProcessorConfig.APT_PACKAGE)
-class WrouterProcessor : AbstractProcessor(){
+class WRouterProcessor : AbstractProcessor(){
     // 操作Element的工具类（类，函数，属性，其实都是Element）
     private var elementTool: Elements? = null
 
@@ -77,8 +74,38 @@ class WrouterProcessor : AbstractProcessor(){
         }
     }
 
-    override fun process(p0: MutableSet<out TypeElement>?, p1: RoundEnvironment?): Boolean {
-        return false
+    override fun process(set: MutableSet<out TypeElement>?, roundEnvironment: RoundEnvironment?): Boolean {
+        if (set == null || set.isEmpty()){
+            messager?.printMessage(Diagnostic.Kind.NOTE, "并没有发现 被@ARouter注解的地方呀")
+            return false
+        }
+        // Activity type
+
+        // Activity type
+        val activityType = elementTool?.getTypeElement(ProcessorConfig.ACTIVITY_PACKAGE)
+        val activityMirror = activityType?.asType()
+        // 获取所有被 @ARouter 注解的 元素集合
+
+        // 获取所有被 @ARouter 注解的 元素集合
+        val elements: Set<Element?>? =
+            roundEnvironment?.getElementsAnnotatedWith(
+                WRouter::class.java
+            )
+        elements?.forEach {
+            // 获取简单类名，例如：MainActivity
+            // 有三处地方使用到注解  for 3
+            // 获取类节点，获取包节点 （com.xiangxue.xxxxxx）
+            // String packageName = elementTool.getPackageOf(element).getQualifiedName().toString();
+
+            // 获取简单类名，例如：MainActivity
+            val className: String = it?.simpleName.toString()
+            messager?.printMessage(
+                Diagnostic.Kind.NOTE,
+                "被@ARetuer注解的类有：$className"
+            ) // 打印出 就证明APT没有问题
+
+        }
+        return true
     }
 
 }
